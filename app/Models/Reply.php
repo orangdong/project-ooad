5195dcb0340b5b0b939e827f85cf76f6a5934e53
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-12-10 17:30:27 +0700

    migrations & models completed

---
+++ app/Models/Reply.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'thread_id', 'description', 'creator_id', 'creator_type'
+    ];
+}
---
+++ database/migrations/2021_12_10_091736_create_ratings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions');
+            $table->foreignId('user_id')->constrained('users');
+            $table->integer('rating')->default(1);
+            $table->string('rating_date');
+            $table->unique(['transaction_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
