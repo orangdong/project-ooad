01deeec4b2b20654dba75eeba13c7a54bfe4d557
jordyf15 <jordyferdian88@gmail.com>
2021-12-15 14:54:40 +0700

    created add service popup in managestore

--- public/js/manageStore.js
+++ public/js/manageStore.js
@@ -4,6 +4,12 @@ addProductBtn.addEventListener('click',()=>{
     createFormProduk();
 });
 
+const addLayananBtn = document.querySelector('#tambah-layanan-btn');
+addLayananBtn.addEventListener('click',()=>{
+    createFormBg();
+    createFormLayanan();
+});
+
 function createFormBg(){
     const body = document.querySelector('body');
 
@@ -94,3 +100,118 @@ function createFormProduk(){
     buttonDiv.appendChild(button);
 }
 
+function createFormLayanan(){
+    const formBg = document.querySelector('#form-bg');
+
+    const formLayanan = document.createElement('form');
+    formLayanan.id = 'formLayanan';
+    formLayanan.addEventListener('click',(e)=>{
+        e.stopPropagation();
+    });
+    formBg.appendChild(formLayanan);
+
+    const formTitle = document.createElement('h2');
+    formTitle.textContent='Tambah Layanan';
+    formLayanan.appendChild(formTitle);
+
+    const namaDiv = document.createElement('div');
+    namaDiv.id = 'containerNama';
+    formLayanan.appendChild(namaDiv);   
+    const namaLabel = document.createElement('label');
+    namaLabel.for='namaLayanan';
+    namaLabel.textContent = 'Nama Layanan';
+    formLayanan.appendChild(namaLabel);
+    const namaInput = document.createElement('input');
+    namaInput.type='text';
+    namaInput.id = 'namaLayanan';
+    namaInput.name='namaLayanan';
+    namaInput.placeholder = 'Masukkan nama layanan';
+    namaDiv.appendChild(namaInput);
+
+    const deskripsiDiv = document.createElement('div');
+    deskripsiDiv.id='containerDeskripsi';
+    formLayanan.appendChild(deskripsiDiv);
+    const deskripsiLabel = document.createElement('label');
+    deskripsiLabel.for = 'deskripsiLayanan';
+    deskripsiDiv.appendChild(deskripsiLabel);
+
+    const deskripsiInput = document.createElement('input');
+    deskripsiInput.type = 'text';
+    deskripsiInput.name='deskripsiLayanan';
+    deskripsiInput.id = 'deskripsiLayanan';
+    deskripsiInput.placeholder = 'Masukkan deskripsi layanan';
+    deskripsiDiv.appendChild(deskripsiInput);
+
+    const hargaKesediaanContainer = document.createElement('div');
+    hargaKesediaanContainer.id = 'harga-kesediaan-container';
+    formLayanan.appendChild(hargaKesediaanContainer);
+
+    const hargaDiv = document.createElement('div');
+    hargaDiv.id = 'harga-container';
+    hargaKesediaanContainer.appendChild(hargaDiv);
+
+    const hargaLabel = document.createElement('label');
+    hargaLabel.id= 'hargaLayanan';
+    hargaLabel.textContent = 'Harga layanan / jam';
+    hargaDiv.appendChild(hargaLabel);
+
+    const hargaInput = document.createElement('input');
+    hargaInput.type = 'text';
+    hargaInput.name='hargaLayanan';
+    hargaInput.id = 'hargaLayanan';
+    hargaInput.placeholder = 'Masukkan harga / jam';
+    hargaDiv.appendChild(hargaInput);
+
+    const kesediaanDiv = document.createElement('div');
+    kesediaanDiv.id = 'kesediaan-layanan-container';
+    hargaKesediaanContainer.appendChild(kesediaanDiv);
+    const kesediaanLabel = document.createElement('label');
+    kesediaanLabel.for = 'kesediaanLayanan';
+    kesediaanLabel.textContent = 'Kesediaan';
+    kesediaanDiv.appendChild(kesediaanLabel);
+
+    const radioDiv = document.createElement('div');
+    radioDiv.id = 'radio-container';
+    kesediaanDiv.appendChild(radioDiv);
+
+    const sediaDiv = document.createElement('div');
+    sediaDiv.class = 'radioItem';
+    sediaDiv.id = 'sedia-container';
+    radioDiv.appendChild(sediaDiv);
+
+    const sediaInput = document.createElement('input');
+    sediaInput.type = 'radio';
+    sediaInput.name = 'kesediaanLayanan';
+    sediaInput.id = 'layananSedia';
+    sediaInput.value = 'sedia';
+    sediaDiv.appendChild(sediaInput);
+
+    const sediaLabel = document.createElement('label');
+    sediaLabel.for = 'layananSedia';
+    sediaLabel.textContent = 'Tersedia';
+    sediaDiv.appendChild(sediaLabel);
+
+    const takSediaDiv = document.createElement('div'); 
+    takSediaDiv.class = 'radioItem';
+    radioDiv.appendChild(takSediaDiv);
+
+    const takSediaInput = document.createElement('input');
+    takSediaInput.type = 'radio';
+    takSediaInput.name='kesediaanLayanan';
+    takSediaInput.id ='layananTidakSedia';
+    takSediaInput.value = 'tidakSedia';
+    takSediaDiv.appendChild(takSediaInput);
+
+    const takSediaLabel = document.createElement('label');
+    takSediaLabel.for = 'layananTidakSedia';
+    takSediaLabel.textContent = 'Tidak';
+    takSediaDiv.appendChild(takSediaLabel);
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.id = 'button-container';
+    formLayanan.appendChild(buttonDiv);
+
+    const button = document.createElement('button');
+    button.textContent = 'Tambah';
+    buttonDiv.appendChild(button);
+}
