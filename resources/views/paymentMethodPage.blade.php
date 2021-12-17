f5b4c250eff53fb906ea186f6c57e2bf6973d25e
Mhyth <leanderjose7@gmail.com>
2021-12-17 23:42:03 +0700

    created payment method page

---
+++ resources/views/paymentMethodPage.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/footer.css">
+        <link rel="stylesheet" href="/css/headermsme.css">
+        <link rel="stylesheet" href="/css/paymentMethodPage.css">
+        <title>Document</title>
+    </head>
+    
+<body>
+    @include('headertourist')
+    <div id='jumbotron'>
+        <h2>Batik nusa</h2>
+        <p>Alamat: Labuan Bajo, Manggarai Barat</p>
+    </div>
+    <div id="judul">
+        <h2>Pilih Metode Pembayaran </h2>
+    </div>
+    <div id="container">
+        <div id="gambar-holder">
+            <div id="satu">
+                <img src="assets/logo-dana.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-ovo.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-bca.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-gopay.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-shoppe.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-qris.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-link.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-doku.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-ran.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-pay.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-indomaret.png" alt="">
+            </div>
+            <div id="satu">
+                <img src="assets/logo-alfamart.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div id="checkout">
+        <a href="">Checkout</a>
+    </div>
+    @include('footer')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('checkout');
+    return view('paymentMethodPage');
 });
 
 Route::middleware(['auth:sanctum'])->get('/dashboard', function () {
